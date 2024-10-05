15d63a1ce5ae5021595e7f1fed284bd889c9afe8
anorekarl <104476707+euphorio1@users.noreply.github.com>
2024-10-05 10:52:25 +0800

    test

--- script.js
+++ script.js
@@ -6,9 +6,11 @@ let myInterval;
 // HTML ELEMENTOS
 const mainBtn = document.getElementById('main-btn')
 const minutes = document.getElementById('minutes')
+const seconds = document.getElementById('seconds')
 const breakOption1 = document.getElementById('break1')
 const breakOption2 = document.getElementById('break2')
 const focus = document.getElementById('focusId')
+const resetBtn = document.getElementById('resetBtn')
 
 
 // MGA TUNOG
@@ -16,58 +18,97 @@ const bellSound = new Audio('/files/mixkit-arcade-score-interface-217.wav')
 const startSound = new Audio('/files/mixkit-retro-arcade-racer-start-218.wav')
 
 // TIMER CONDITIOn
-let condition = true;
+let condition;
+let menuState = false;
 
 // MENU CONDITIONS
-let focusState = true;
+let focusState;
 let break1State;
 let break2State;
 
+// Reset condition
+let reset;
 
 
+resetBtn.addEventListener('click', function(){
 
+    resetTimer();
+
+})
+
+
+// MENU BUTTONS
  focus.addEventListener('click', function () {
-
     focusState = true
      break1State = false
      break2State = false
-    minutes.innerText = 25;
 
+    if (menuState) {
+    focus.stopPropagation();
+    focus.preventDefault();
+}
     if (focusState){
+          resetTimer();
         focus.style.backgroundColor = "#53ecb7"
         breakOption1.style.backgroundColor = "#FBFADA"
         breakOption2.style.backgroundColor = "#FBFADA"
+        focus.stopPropagation();
+        focus.preventDefault();
     }
+
+    
+    
 })
 
 breakOption1.addEventListener('click', function () {
-
-    focusState = false
+     focusState = false
     break1State = true
     break2State = false
-    minutes.innerText = 5;
+
+    if (menuState){
+        breakOption1.stopPropagation();
+        breakOption1.preventDefault();
+    }
+
 
     if (break1State){
+        resetTimer();
         focus.style.backgroundColor = "#FBFADA"
         breakOption1.style.backgroundColor = "#53ecb7"
         breakOption2.style.backgroundColor = "#FBFADA"
+        breakOption1.stopPropagation();
+        breakOption1.preventDefault();
     }
+
+    
 })
 
 breakOption2.addEventListener('click', function () {
-
-    focusState = false
+      focusState = false
     break1State = false
     break2State = true
-    minutes.innerText = 10;
+   
+
+    if (menuState){
+        breakOption2.stopPropagation();
+        breakOption2.preventDefault();
+    }
+
+  
 
     if (break2State){
+        resetTimer();
         focus.style.backgroundColor = "#FBFADA"
         breakOption1.style.backgroundColor = "#FBFADA"
         breakOption2.style.backgroundColor = "#53ecb7"
+        breakOption2.stopPropagation();
+        breakOption2.preventDefault();
     }
+    
 })
 
+
+// Init
 
 
 mainBtn.addEventListener('click', function() {
@@ -77,12 +118,15 @@ mainBtn.addEventListener('click', function() {
     if (mainBtn.innerText == 'start'){
         startSound.play()
         mainBtn.innerText = "pause"
+        condition = true;
+        menuState = true;
         timer()
         
 
        }else if(mainBtn.innerText == "pause"){
 
         mainBtn.innerText = "resume"
+
         pause()
 
        }else if (mainBtn.innerText == "resume"){
@@ -90,9 +134,11 @@ mainBtn.addEventListener('click', function() {
         mainBtn.innerText = "pause"
         pauseResume()
        }
-    
 }
 )
+
+
+// TIMER SETTINGS
  
 setCount = 0;
 
@@ -154,6 +200,28 @@ function pauseResume(){
     }
 }
 
+function resetTimer(){
+
+    menuState = false;
+
+    mainBtn.innerText = 'start';
+   
+    if (focusState){
+        minutes.innerText = 25;
+        seconds.innerText = "00";
+        clearInterval(myInterval);
+    }else if(break1State){
+         minutes.innerText ="0" + 5;
+        seconds.innerText = "00";
+        clearInterval(myInterval);
+    }else if (break2State){
+          minutes.innerText = 10;
+        seconds.innerText = "00";
+        clearInterval(myInterval);
+    }
+    
+
+}
 
 
 
